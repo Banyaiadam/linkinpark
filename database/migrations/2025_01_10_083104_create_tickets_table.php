6d17d0be512ceef017eab55c70ca7f7fb97405c8
Banyaiadam <77103062+Banyaiadam@users.noreply.github.com>
2025-01-10 10:43:38 +0100

    ketto

--- database/migrations/2025_01_10_083104_create_tickets_table.php
+++ database/migrations/2025_01_10_083104_create_tickets_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use function Laravel\Prompts\table;
+
 return new class extends Migration
 {
     /**
@@ -13,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('artist_id');
+            $table->foreignId('sector_id');
+            $table->foreignId('location_id');
+            $table->integer('count');
+            $table->date('date');
+            $table->decimal('price');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
